d35992f3182654b69c93218b30f6433cd4d367da
Dreamy Pujara <42131610+DreamyPujara@users.noreply.github.com>
2023-06-04 09:58:46 +0530

    Create pascals_triangle.cpp

---
+++ pascals_triangle.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+vector<vector<long long int>> printPascal(int n) 
+
+{
+
+  vector<vector<long long int>> ans(n);
+
+ 
+
+  for(int i = 1; i<=n; i++){
+
+    long long int c = 1;
+
+   for(int j = 1 ; j<=i;j++){
+
+       
+
+       ans[i-1].push_back(c);
+
+       c = c*(i -j )/j;
+
+   }
+
+  }
+
+ 
+
+  return ans;
+
+}
